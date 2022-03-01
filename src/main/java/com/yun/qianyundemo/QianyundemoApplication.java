d98d35c055d05f8b72ef705ff43a6467582669af
qiantao <qiantao112@163.com>
2022-03-01 16:18:59 +0800

    第一次提交

---
+++ src/main/java/com/yun/qianyundemo/QianyundemoApplication.java
@@ -0,0 +1,13 @@
+package com.yun.qianyundemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QianyundemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QianyundemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yun/qianyundemo/QianyundemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.qianyundemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QianyundemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
